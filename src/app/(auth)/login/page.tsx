bc45f05218564081b2da5fe7fdb43572b46f75b4
noahlabear <n.leberre@student.vu.nl>
2024-07-20 20:38:57 +0200

    chore: Add zod library for form validation and update dependencies

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+"use client";
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormSchema } from '@/lib/types'; // Might need to change this path to be more specific
 
 const LoginPage = () => {
-  return (
-    <div>login</div>
-  )
+    const router = useRouter();
+    const [submitError, setSubmitError] = useState('');
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        mode: 'onChange',
+        resolver: zodResolver(FormSchema),
+        defaultValues: { email: '', password: '' },    
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+        formData
+    ) => {};
+
+    return <div>login</div>
 }
 
 export default LoginPage;
