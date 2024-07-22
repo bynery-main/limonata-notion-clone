99b24b38dbc4d3338010a0096f4100246c99fc2b
noahlabear <n.leberre@student.vu.nl>
2024-07-22 13:21:50 +0200

    added firebase tools and functions

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import React from "react";
-import { auth } from "../../../../firebase/firebaseConfig";
+import { auth, db } from "../../../../firebase/firebaseConfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
+import { collection, getDocs } from "firebase/firestore";
+
+async function fetchData() {
+  const querySnapshot = await getDocs(collection(db, ""));
+}
 
 const Dashboard = () => {
 
@@ -15,7 +20,7 @@ const Dashboard = () => {
       return router.push('/login');
     }
 
-
+    
 
     return (
         <div>
