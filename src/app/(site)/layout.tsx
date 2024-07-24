1ef0127f7de9e6bef686474740fdeb2132130ea7
ovi <ovirandunu@gmail.com>
2024-07-24 17:37:51 +0200

    remove junk header from homepage

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
       {children}
     </main>
   );
