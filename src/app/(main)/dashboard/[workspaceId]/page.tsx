aeb7a1e2d316b8b5220bdd375f00a54a0ffb65d8
ovi <ovirandunu@gmail.com>
2024-07-25 01:45:02 +0200

    refactor random delete account function from workpace page.tsx to misc.ts

--- src/app/(main)/dashboard/[workspaceId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/page.tsx
@@ -1,35 +1,9 @@
 import React from "react";
-import { auth } from '@/firebase/firebaseConfig';
-import { deleteUser, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
+// import { auth } from '@/firebase/firebaseConfig';
+// import { deleteUser, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
 
 const Page = () => {
-    const handleDeleteAccount = () => {
-        if (confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
-            const user = auth.currentUser;
 
-            if (user) {
-                deleteUser(user).then(() => {
-                    alert("Account deleted successfully.");
-                    // Redirect the user to the login page after account deletion
-                    window.location.href = "/login";
-                }).catch(async (error: { code: string; message: string; }) => {
-                    console.error("Error deleting account:", error);
-                    if (error.code === 'auth/requires-recent-login') {
-                        alert("You need to reauthenticate before deleting your account. Please log in again.");
-                        // Example: Re-authenticate user
-                        const credential = EmailAuthProvider.credential(
-                            user.email!,
-                            prompt('Please enter your password again:')!
-                        );
-                        await reauthenticateWithCredential(user, credential);
-                        handleDeleteAccount();
-                    } else {
-                        alert("Error deleting account: " + error.message);
-                    }
-                });
-            }
-        }
-    };
 
     return (
         <div>
