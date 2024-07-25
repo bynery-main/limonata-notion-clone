b05d8862d1f68aeb4c60a3ce1eb0472472eeca36
titorossi <titotr22@gmail.com>
2024-07-25 23:56:11 +0200

    added toast messages to delete account feature

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import { auth } from "@/firebase/firebaseConfig";
 import { deleteUser } from 'firebase/auth';
 import ConfirmationModal from '@/components/settings/ConfirmationModal';
+import { Toaster, toast } from 'react-hot-toast';
 
 const SettingsPage = () => {
   const router = useRouter();
@@ -13,22 +14,22 @@ const SettingsPage = () => {
   const handleDeleteAccount = async () => {
     const user = auth.currentUser;
     if (!user) {
-      alert('No user is currently signed in.');
+      toast.error('No user is currently signed in.');
       return;
     }
 
     try {
       await deleteUser(user);
-      alert('Account deleted successfully.');
+      toast.success('Account deleted successfully.');
       router.push('/login'); // Redirect to login page after account deletion
     } catch (error: any) {
       if (error.code === 'auth/requires-recent-login') {
-        alert('Please re-login and try again.');
+        toast.error('Please re-login and try again.');
         // Redirect to login page or re-authenticate the user here
         router.push('/login');
       } else {
         console.error('Error deleting account:', error);
-        alert('Error deleting account. Please try again.');
+        toast.error('Error deleting account. Please try again.');
       }
     }
   };
@@ -39,6 +40,7 @@ const SettingsPage = () => {
 
   return (
     <div>
+      <Toaster />
       <h1>Settings</h1>
       <button
         onClick={() => setShowModal(true)}
