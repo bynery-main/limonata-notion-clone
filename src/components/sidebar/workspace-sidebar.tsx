c2214c33ff808b89d0144593e0203daafda25fcf
noahlabear <n.leberre@student.vu.nl>
2024-07-26 17:53:24 +0200

    Added native navigation

--- src/components/sidebar/workspace-sidebar.tsx
+++ src/components/sidebar/workspace-sidebar.tsx
@@ -1,13 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import NativeNavigation from "./native-navigation";
 
 interface WorkspaceSidebarProps {
     params: {workspaceId: string};
     className?: string;
 }
 
-const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({params, className}) => {
+const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({
+    params, 
+    className
+}) => {
     return (
-        <div>WorkspaceSidebar</div>
+        <NativeNavigation params={params} className={twMerge('my-2', className)} />
+        
     );
 }
 
