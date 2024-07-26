f395ebb9463ec67955d5fe3c5fae8168a6e694a3
ovi <ovirandunu@gmail.com>
2024-07-26 22:19:04 +0200

    animate on worksapce load from mainsidebar

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,16 +1,39 @@
+"use client";
+
 import { MainSidebar } from "@/components/sidebar/main-sidebar";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from 'next/navigation';
+
+const AnimatedChildren: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsAnimating(true);
+    const timer = setTimeout(() => setIsAnimating(false), 500); // Match this to your animation duration
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  return (
+    <div
+      className={`w-full ${isAnimating ? 'slide-top' : ''}`}
+      style={{ overflow: 'hidden' }}
+    >
+      {children}
+    </div>
+  );
+};
 
 interface LayoutProps {
   children: React.ReactNode;
   params: any;
 }
 
-const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
+const Layout: React.FC<LayoutProps> = ({ children, params }) => {
   return (
-    <main className="flex over-hidden h-screen">
+    <main className="flex overflow-hidden h-screen">
       <MainSidebar />
-      {children}
+      <AnimatedChildren>{children}</AnimatedChildren>
     </main>
   );
 };
