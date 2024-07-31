bd4034533050cb85ae974e31186968d6629b8206
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2024-07-31 15:22:21 -0400

    Moved Quillboy to FileID.jsx

--- src/app/(main)/dashboard/[workspaceId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/page.tsx
@@ -1,6 +1,5 @@
 export const dynamic = 'force-dynamic';
 
-import QuillEditor from '@/components/quill-editor/quill-editor';
 import { getWorkspaceDetails } from '@/lib/queries/queries';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -10,11 +9,7 @@ const Workspace = async ({ params }: { params: { workspaceId: string } }) => {
   if (error || !data) redirect('/dashboard');
   return (
     <div className="relative">
-      <QuillEditor
-        dirType="workspace"
-        fileId={params.workspaceId}
-        dirDetails={data || {}}
-      />
+
     </div>
   );
 };
