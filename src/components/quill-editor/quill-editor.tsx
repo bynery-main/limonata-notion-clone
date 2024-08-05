a68ee5a080a62da14dcbd266fd79b3236a1a0dae
noahlabear <n.leberre@student.vu.nl>
2024-08-05 22:09:53 +0200

    Can add text to notes

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -1,7 +1,9 @@
-"use client";
+"use client"
 
 import React, { useCallback, useEffect, useState, useRef, useMemo } from "react";
 import "quill/dist/quill.snow.css";
+import { db } from "@/firebase/firebaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 interface QuillEditorProps {
   dirDetails: any;
@@ -9,49 +11,35 @@ interface QuillEditorProps {
   fileId: string;
 }
 
-var TOOLBAR_OPTIONS = [
-  ["bold", "italic", "underline", "strike"], // toggled buttons
+const TOOLBAR_OPTIONS = [
+  ["bold", "italic", "underline", "strike"],
   ["blockquote", "code-block"],
-
-  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ header: 1 }, { header: 2 }],
   [{ list: "ordered" }, { list: "bullet" }],
-  [{ script: "sub" }, { script: "super" }], // superscript/subscript
-  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-  [{ direction: "rtl" }], // text direction
-
-  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ script: "sub" }, { script: "super" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ direction: "rtl" }],
+  [{ size: ["small", false, "large", "huge"] }],
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ color: [] }, { background: [] }],
   [{ font: [] }],
   [{ align: [] }],
-
-  ["clean"], // remove formatting button
+  ["clean"],
 ];
 
-const QuillEditor: React.FC<QuillEditorProps> = ({
-  dirType,
-  fileId,
-  dirDetails,
-}) => {
+const QuillEditor: React.FC<QuillEditorProps> = ({ dirType, fileId, dirDetails }) => {
   const [quill, setQuill] = useState<any>(null);
   const wrapperRef = useRef<HTMLDivElement | null>(null);
 
   const details = useMemo(() => {
-    let selectedDir;
+    return {
+      workspaceId: dirDetails.workspaceId,
+      folderId: dirDetails.folderId,
+      fileId: fileId
+    };
+  }, [dirDetails]);
 
-    if (dirType === "file") {
-
-    }
-
-    if (dirType === "folder") {
-
-    }
-
-    if (dirType === "workspace") {
-
-    }
-  }, [])
+  console.log("Details: ", dirDetails);
 
   useEffect(() => {
     if (typeof window === "undefined") return;
@@ -69,22 +57,55 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
         },
       });
       setQuill(q);
+
+      q.on("text-change", async () => {
+        const text = q.root.innerHTML;
+        try {
+
+          if (details && details.workspaceId && details.folderId && details.fileId) {
+            const fileDocRef = doc(db, "workspaces", details.workspaceId, "folders", details.folderId, "notes", details.fileId);
+            await setDoc(fileDocRef, { text }, { merge: true });
+            console.log("Document successfully updated!");
+          }
+        } catch (error) {
+          console.log("Error updating document: ", error);
+        }
+      });
     };
 
     initQuill();
-  }, []);
+  }, [details]);
+
+  useEffect(() => {
+    if (!fileId || !quill) return;
+
+    const fetchInformation = async () => {
+      if (dirType === "file") {
+        const fileDocRef = doc(db, "workspaces", dirDetails.workspaceId, "folders", dirDetails.folderId, "notes", fileId);
+        const fileDoc = await getDoc(fileDocRef);
+
+        if (fileDoc.exists()) {
+          const fileData = fileDoc.data();
+          quill.setContents(fileData.content || {});
+          console.log("Document data:", fileData);
+        } else {
+          console.log("No such document!");
+        }
+      }
+    };
+
+    fetchInformation();
+  }, [fileId, dirType, quill, dirDetails]);
 
   return (
-    <div
-      className="
-        flex
-        justify-center
-        items-center
-        flex-col
-        mt-2
-        relative
-        "
-    >
+    <div className="
+      flex
+      justify-center
+      items-center
+      flex-col
+      mt-2
+      relative
+    ">
       <div id="container" className="max-w-[800px]" ref={wrapperRef}></div>
     </div>
   );
