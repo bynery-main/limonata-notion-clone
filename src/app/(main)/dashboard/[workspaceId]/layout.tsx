16667dd47bd651718a666e89ac13b80f12b948dd
ovi <ovirandunu@gmail.com>
2024-08-29 19:56:39 +0200

    remove unused code

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -39,10 +39,6 @@ interface BreadcrumbItem {
   icon: React.ReactNode;
 }
 
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
-
 const Layout: React.FC<LayoutProps> = ({ children, params }) => {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const [emoji, setEmoji] = useState<string>("🍋"); // Default emoji
