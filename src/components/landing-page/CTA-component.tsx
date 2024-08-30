d2dcd58b93149108faef5ab4ca17dd9b28998cd2
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2024-08-30 14:19:01 -0700

    refactor: Update CTA component to handle user authentication and navigation

--- src/components/landing-page/CTA-component.tsx
+++ src/components/landing-page/CTA-component.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
 import FancyText from '@carefully-coded/react-text-gradient';
 import { Button } from "@/components/ui/button";
 import styled, { keyframes } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase/firebaseConfig"; // Adjust the path if necessary
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const gradientAnimation = keyframes`
     0% {
@@ -34,6 +37,40 @@ const AnimatedButton = styled(Button)`
 `;
 
 export default function CTAComponent() {
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsSignedIn(true);
+      } else {
+        setIsSignedIn(false);
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  const provider = new GoogleAuthProvider();
+
+  const login = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const goToDashboard = () => {
+    router.push("/dashboard");
+  };
+
+
   return (
     <div className="flex flex-col items-center justify-center text-center px-4 sm:px-6 md:px-8 my-16 sm:my-30 md:my-40 lg:my-50 mx-auto max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-3xl">
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight text-black">
@@ -46,14 +83,20 @@ export default function CTAComponent() {
         </FancyText>
         {' '}be at the service of your learning.
       </h2> 
-      <AnimatedButton 
-        className="mt-8 sm:mt-10 rounded-full text-base sm:text-md md:text-md lg:text-lg font-light px-6 py-4 sm:px-8 sm:py-5" 
-        onClick={() => console.log("Clicked!")}
-      >
-        Get Started
-      </AnimatedButton>
+      <div className=" mt-7 space-y-4 md:space-y-0 md:space-x-4 ">
+
+      {!isSignedIn ? (
+            <AnimatedButton variant="default" onClick={login} className=" md:w-auto">
+              Start a Workspace
+            </AnimatedButton>
+          ) : (
+            <AnimatedButton variant="default" onClick={goToDashboard} className=" md:w-auto">
+              Go to Your Dashboard
+            </AnimatedButton>
+          )}
+          </div>
       <p className="text-xs sm:text-sm md:text-base text-gray-500 mt-3">
-        Yes. It's free.
+        Yes. It&apos;s free.
       </p>
     </div>
   );
