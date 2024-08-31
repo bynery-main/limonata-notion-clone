307926141a86b93e21c6c57b4774f8145c0984af
ovi <ovirandunu@gmail.com>
2024-08-31 11:04:48 +0200

    remove unused platform prop

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,27 +1,16 @@
 "use client";
 
 import Footer from "@/components/footer/footer";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
-  const [platform, setPlatform] = useState<"desktop" | "mobile">("desktop");
 
-  useEffect(() => {
-    const updatePlatform = () => {
-      setPlatform(window.innerWidth <= 768 ? "mobile" : "desktop");
-    };
-
-    window.addEventListener("resize", updatePlatform);
-    updatePlatform(); 
-
-    return () => window.removeEventListener("resize", updatePlatform);
-  }, []);
 
   return (
     <main>
       {/* <Header /> */}
       {children}
-      <Footer platform={platform} />
+      <Footer  />
     </main>
   );
 };
