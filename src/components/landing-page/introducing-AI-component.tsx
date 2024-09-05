4f72e6dc240ff5a134ffe5cca0a77e41f35f3c11
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2024-09-04 19:17:25 -0700

    Added lottie animation

--- src/components/landing-page/introducing-AI-component.tsx
+++ src/components/landing-page/introducing-AI-component.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import AIImage from '../../../public/Images/AI.png';
 import FancyText from '@carefully-coded/react-text-gradient';
-
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import AIAnimation from '../../../public/Images/AILottieExport.json';
 export default function IntroductionComponent() {
   return (
-    <div className="w-full max-w-[90vw] lg:max-w-[80vw] xl:max-w-[1400px] mx-auto my-16 sm:my-24 md:my-32 lg:my-40 px-4 sm:px-10 md:px-40 lg:px-40">
+    <div className="w-full max-w-[90vw] lg:max-w-[80vw] xl:max-w-[1400px] mx-auto px-4 sm:px-10 md:px-40 lg:px-40">
       <div className="flex flex-col-reverse md:flex-row md:items-center md:space-x-8 lg:space-x-16">
-        <div className="w-full md:w-1/2 flex flex-col justify-center space-y-4 sm:space-y-6 mt-8 md:mt-0">
+        <div className="w-full md:w-1/2 flex flex-col justify-center space-y-4 sm:space-y-6">
           <h2 className="sm:text-4xl md:text-4xl lg:text-5xl leading-tight">
             Introducing the{' '}
             <FancyText 
@@ -29,14 +30,12 @@ export default function IntroductionComponent() {
             </a>
           </p>
         </div>
-        <div className="w-full md:w-1/2 relative">
-          <div className="w-full aspect-square md:-mt-16 lg:-mt-24 xl:-mt-32">
-            <Image 
-              src={AIImage} 
-              alt="AI Image" 
-              layout="fill"
-              objectFit="contain"
-              priority
+        <div className="w-[100%] h-auto relative bg-blue-200 -my-100">
+        <div className="  -ml-80 overflow-visible">
+          <DotLottieReact       
+            src='https://lottie.host/29673738-770c-4b7e-9d9f-6733b3c4c6e7/8KYAf1OB8n.json'
+            loop
+            autoplay
             />
           </div>
         </div>
