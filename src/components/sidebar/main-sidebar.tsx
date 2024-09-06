1dfb2ecbbc9d207949b37863b88731b7244063ec
ovi <ovirandunu@gmail.com>
2024-09-06 13:19:30 +0200

    refactor: Extract WorkspaceIcon component from MainSidebar into mocular component

--- src/components/sidebar/main-sidebar.tsx
+++ src/components/sidebar/main-sidebar.tsx
@@ -11,6 +11,7 @@ import { Home } from "lucide-react";
 import { onSnapshot, collection, query, where, QuerySnapshot, DocumentData, DocumentChange, doc, getDoc, getFirestore } from "firebase/firestore";
 import { db } from "@/firebase/firebaseConfig";
 import { motion, AnimatePresence } from "framer-motion";
+import WorkspaceIcon from "./workspace-icon";
 
 export const MainSidebar = (): JSX.Element => {
   const { user } = useAuth();
@@ -244,39 +245,3 @@ export const MainSidebar = (): JSX.Element => {
   );
 };
 
-interface WorkspaceIconProps {
-  isActive: boolean;
-  workspace: Workspace;
-  index: number;
-  onClick: () => void;
-  emoji: string | undefined;
-}
-
-const WorkspaceIcon: React.FC<WorkspaceIconProps> = ({ workspace, index, onClick, isActive, emoji }) => {
-  return (
-    <motion.div
-      className={`relative mt-4 w-10 h-10 rounded-lg overflow-hidden cursor-pointer flex items-center justify-center text-white font-semibold text-md`}
-      onClick={onClick}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      layout
-    >
-      <motion.div
-        className={`absolute inset-0 ${isActive ? 'bg-black' : 'bg-[#666666]'}`}
-        layoutId={`workspace-bg-${workspace.id}`}
-      />
-      <motion.div
-        className={`absolute inset-0 rounded-lg border-2 ${isActive ? 'border-white' : 'border-transparent'}`}
-        layoutId={`workspace-border-${workspace.id}`}
-      />
-      <motion.span
-        className="relative z-10"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        {emoji || workspace.name.charAt(0).toUpperCase()}
-      </motion.span>
-    </motion.div>
-  );
-};
