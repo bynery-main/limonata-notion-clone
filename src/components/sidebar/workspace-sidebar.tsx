8b7a786953ecdea0bc63a1688dfec17e7ac43870
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2024-09-09 22:01:24 -0700

    Made Go Pro button a bit smaller

--- src/components/sidebar/workspace-sidebar.tsx
+++ src/components/sidebar/workspace-sidebar.tsx
@@ -296,7 +296,7 @@ const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({
         {(tier === "free" || subscriptionStatus === "active_pending_cancellation") && (
           <Button
             onClick={() => setShowGoProModal(true)}
-             className="mx-4 mt-4 shadow-lg inline-flex text-white h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+             className="mx-4 mt-4 shadow-lg inline-flex text-white h-10 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
 
           >
             {subscriptionStatus === "active_pending_cancellation" ? "Resubscribe" : "Go Pro"}
