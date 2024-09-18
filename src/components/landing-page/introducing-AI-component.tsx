ac8f274bb0284e30b2232c7eb0d64271bcf264a3
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2024-09-18 01:11:34 -0700

    Added Lottie Animation

--- src/components/landing-page/introducing-AI-component.tsx
+++ src/components/landing-page/introducing-AI-component.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import AIImage from '../../../public/Images/AI.png';
 import FancyText from '@carefully-coded/react-text-gradient';
-
+import Lottie from 'lottie-react';
+import InputTypes from '../../../public/Images/Input-Types.json';
 export default function IntroductionComponent() {
   return (
     <div className="w-full max-w-[90vw] lg:max-w-[80vw] xl:max-w-[1400px] mx-auto my-16 sm:my-24 md:my-20 lg:my-28 px-4 sm:px-10 md:px-40 lg:px-40">
@@ -18,9 +19,11 @@ export default function IntroductionComponent() {
             </FancyText>
             {' '}to your learning.
           </h2>
-          <p className="text-base sm:text-lg md:text-lg lg:text-lg">
-            You can add study notes as images, links, notes, quotes,
-            PDFs, articles, any study resource from the web, or your computer.
+          <p className="text-base sm:text-lg md:text-lg lg:text-lg -mr-20">
+            Don't  waste time creating study resources or <b>prompting ChatGPT. </b>
+            <br /> <br />
+            In Limonata, you can add your own and your peer&apos;s study notes. All
+            of this will be by your AI used to generate  <b>personalized study materials.</b>
           </p>
           <p className="text-base sm:text-lg md:text-lg lg:text-lg">
             And don&apos;t forget,{" "}
@@ -31,6 +34,7 @@ export default function IntroductionComponent() {
         </div>
         <div className="w-full md:w-1/2 relative">
           <div className="w-full aspect-square md:-mt-16 lg:-mt-24 xl:-mt-32">
+           {/* Static Image
             <Image 
               src={AIImage} 
               alt="AI Image" 
@@ -38,8 +42,17 @@ export default function IntroductionComponent() {
               objectFit="contain"
               priority
             />
-          </div>
-        </div>
+            */}
+              <div className="relative w-full h-full flex items-center justify-center pl-20 mt-20">
+                <Lottie 
+                  animationData={InputTypes} 
+                  loop={true} 
+                  className="min-w-[800px] h-[800px] -ml-20 "
+                />
+              </div>       
+              </div>       
+
+               </div>
       </div>
     </div>
   );
