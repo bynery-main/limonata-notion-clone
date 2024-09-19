a360785e32d62e86cc99efd3206cc1a0c169459e
ovi <ovirandunu@gmail.com>
2024-09-20 01:24:17 +0200

    Refactor layout component: Add background gradient and adjust max height

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -7,6 +7,7 @@ import React from "react";
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       <div className="max-h-[10vw]">
         <Navbar />
       </div>
