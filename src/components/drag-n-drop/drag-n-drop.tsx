f08f726d5863fee35224473d7fd93a73609a3e5a
ovi <ovirandunu@gmail.com>
2024-09-19 20:29:27 +0200

    Refactor drag-n-drop component and file uploader: Update file upload instructions

--- src/components/drag-n-drop/drag-n-drop.tsx
+++ src/components/drag-n-drop/drag-n-drop.tsx
@@ -320,10 +320,10 @@ const FileUploader: React.FC<FileUploaderProps> = ({
               <p className="text-center text-md mb-2">
                 Drag and drop a file here, or click to select a file
               </p>
-              <p className="text-center text-sm text-gray-500 italic">
-                (Psst! If you&apos;re new here, make sure to create a folder
+              <p className="text-center text-md text-gray-500 italic">
+                Psst! If you&apos;re new here, make sure to create a folder
                 first. It&apos;s like making a comfy bed for your files before
-                tucking them in!)
+                tucking them in!
               </p>
             </div>
           )}
