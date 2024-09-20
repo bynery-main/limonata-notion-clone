4e2c918c562f8317eff3cafd631257913ec66e38
ovi <ovirandunu@gmail.com>
2024-09-20 02:13:58 +0200

    Refactor NavbarAuthButton component: Adjust button height and padding

--- src/components/ui/bm-button.tsx
+++ src/components/ui/bm-button.tsx
@@ -9,7 +9,7 @@ interface NavbarAuthButtonProps {
 const NavbarAuthButton: React.FC<NavbarAuthButtonProps> = ({ children, onClick }) => {
   return (
     <button
-      className="inline-flex text-md items-center justify-center h-[3.5vw] px-10 py-0 font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-orange-500 border-solid rounded-full cursor-pointer select-none hover:text-gray-100 hover:border-orange-300 focus:shadow-xs focus:no-underline"
+      className="inline-flex text-md items-center justify-center h-[3vw] px-7 py-0 font-semibold text-center text-gray-300 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-orange-500 border-solid rounded-full cursor-pointer select-none hover:text-gray-100 hover:border-orange-200 focus:shadow-xs focus:no-underline"
       onClick={onClick}
     >
       {children}
