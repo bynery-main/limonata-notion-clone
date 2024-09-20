6dc3bb3406dba399dd57104044dd8dc94b0a9849
noahlabear <n.leberre@student.vu.nl>
2024-09-20 21:13:50 +0200

    Apos error fix

--- src/components/file-handling/powerpoint-display.tsx
+++ src/components/file-handling/powerpoint-display.tsx
@@ -12,10 +12,10 @@ const PowerpointDisplay: React.FC<PowerpointDisplayProps> = ({ fileUrl, fileName
   return (
     <div className="powerpoint-display mb-2">
       <p className="mb-2">
-        Sorry! We can't handle powerpoints right now but you can download the file below. You can upload this as a pdf though!
+        Sorry! We can&apos;t handle powerpoints right now but you can download the file below. You can upload this as a pdf though!
       </p>
-      <a 
-        href={fileUrl} 
+      <a
+        href={fileUrl}
         download={displayFileName}
         className="text-blue-500 hover:text-blue-700 underline"
       >
