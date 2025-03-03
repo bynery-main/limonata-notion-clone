826997b8967b44e1677266e74b6185d90a7dd577
rocofernandezpagella@gmail.com <46428180+Rockstar2K@users.noreply.github.com>
2025-03-03 10:01:46 -0300

    reviews section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,14 +2,12 @@
 
 import { useRouter } from "next/navigation";
 import HeroComponent from "@/components/landing-page/Hero/Hero";
-import Intro from "@/components/landing-page/introduction-component";
-import { TabsDemo } from "@/components/landing-page/landing-page-tabs";
-import AIIntro from "@/components/landing-page/introducing-AI-component";
+
 import CTA from "@/components/landing-page/CTA/CTA";
 import Navbar from "@/components/landing-page/UI/NavBar/NavBar";
 import UpgradeSection from "@/components/landing-page/Upgrade/UpgradeSection";
 import ReadySection from "@/components/landing-page/Ready/ReadySection";
-
+import Reviews from "@/components/landing-page/Reviews/Reviews";
 export default function Home() {
   const router = useRouter();
 
@@ -19,7 +17,7 @@ export default function Home() {
       <HeroComponent />
       <UpgradeSection />
       <ReadySection />
-      
+      <Reviews />
       
 
       <div>
