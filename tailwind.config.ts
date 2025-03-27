633ad2081a145db3ff37cd28c16afa4118627b3e
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2025-03-27 23:11:01 +0100

    Fix Webpack build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import { PluginAPI } from 'tailwindcss/types/config';
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 const {
@@ -7,7 +6,7 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -90,19 +89,16 @@ const config = {
   },
   plugins: [
     require("tailwindcss-animate"),
-    addVariablesForColors,
+    function addVariablesForColors({ addBase, theme }: any) {
+      let allColors = flattenColorPalette(theme("colors"));
+      let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+      );
+      addBase({
+        ":root": newVars,
+      });
+    },
   ],
 } satisfies Config;
-
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }: PluginAPI) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars: Record<string, string> = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val as string])
-  );
-  addBase({
-    ":root": newVars,
-  });
-}
 
 export default config;
